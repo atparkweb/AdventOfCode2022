a4d2eedab6f0eb6b01dfee2c5da39fd3713906e0
Andy Park <atpark333@gmail.com>
2023-01-07 22:19:43 +0900

    Add solution for Day 7 part 1

--- AdventOfCode2022/Properties/Resources.Designer.cs
+++ AdventOfCode2022/Properties/Resources.Designer.cs
@@ -433,7 +433,30 @@ namespace AdventOfCode2022.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg.
+        ///   Looks up a localized string similar to $ cd /
+        ///$ ls
+        ///dir a
+        ///14848514 b.txt
+        ///8504156 c.dat
+        ///dir d
+        ///$ cd a
+        ///$ ls
+        ///dir e
+        ///29116 f
+        ///2557 g
+        ///62596 h.lst
+        ///$ cd e
+        ///$ ls
+        ///584 i
+        ///$ cd ..
+        ///$ cd ..
+        ///$ cd d
+        ///$ ls
+        ///4060174 j
+        ///8033020 d.log
+        ///5626152 d.ext
+        ///7214296 k
+        ///.
         /// </summary>
         internal static string test {
             get {
