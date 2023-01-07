3b0e5456c39d503cf3451619099ead8725638e0e
Andy Park <atpark333@gmail.com>
2023-01-07 22:19:43 +0900

    Add solution for Day 7 part 1

--- AdventOfCode2022/Solutions/DaySeven.cs
+++ AdventOfCode2022/Solutions/DaySeven.cs
@@ -13,6 +13,12 @@ namespace AdventOfCode2022.Solutions
 
         public override void Run()
         {
+            var maxDepth = 0;
+            var currentDepth = 0;
+
+            dirStack.Push("root");
+            directories["root"] = 0;
+
             foreach (var line in lines)
             {
                 if (line.Contains("$ cd"))
@@ -20,18 +26,23 @@ namespace AdventOfCode2022.Solutions
                     if (line.Contains("$ cd .."))
                     {
                         dirStack.Pop();
+                        currentDepth = Math.Max(0, currentDepth - 1);
                     }
                     else if (line.Contains("$ cd /"))
                     {
-                        while (dirStack.Count > 0)
+                        while (dirStack.Count > 1)
                         {
                             dirStack.Pop();
                         }
+                        currentDepth = 0;
                     }
                     else
                     {
                         var dirName = line.Split(" ")[2];
                         dirStack.Push(dirName);
+                        
+                        currentDepth += 1;
+                        maxDepth = Math.Max(maxDepth, currentDepth);
 
                         var currentPath = string.Join("/", dirStack.ToArray());
 
@@ -49,10 +60,41 @@ namespace AdventOfCode2022.Solutions
                 }
             }
 
+            currentDepth = maxDepth;
+            while (currentDepth > 0)
+            {
+                foreach (var key in directories.Keys)
+                {
+                    Stack<string> pathArray = new Stack<string>(key.Split('/'));
+                    if (pathArray.Count == currentDepth)
+                    {
+                        int size = directories[key];
+                        pathArray.Pop();
+                        string parent = string.Join('/', pathArray.ToArray());
+                        if (directories.ContainsKey(parent))
+                        {
+                            directories[parent] += size;
+                        }
+                    }
+                }
+                currentDepth -= 1;
+            }
+
+            int MAX_DIR_SIZE = 100000;
             foreach (var key in directories.Keys)
             {
-                Console.WriteLine(directories[key].ToString());
+                int size = directories[key];
+                if (size <= MAX_DIR_SIZE)
+                {
+                    result += size;
+                }
             }
+
+            int[] results = new int[2];
+            results[0] = result;
+            results[1] = 0;
+
+            Print(7, results);
         }
     }
 }
